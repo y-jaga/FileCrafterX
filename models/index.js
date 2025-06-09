3a4c42cba2df362eff336b15e9610ee85df53605
y-jaga <yogeshjaga123@gmail.com>
2025-06-09 08:09:39 +0530

    modified sequelize instance creation

--- models/index.js
+++ models/index.js
@@ -10,16 +10,29 @@ const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(
+//     config.database,
+//     config.username,
+//     config.password,
+//     config
+//   );
+// }
+
+sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    host: config.host,
+    dialect: "postgres",
+    port: config.port,
+  },
+  config
+);
 
 fs.readdirSync(__dirname)
   .filter((file) => {
