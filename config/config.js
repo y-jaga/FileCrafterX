f60de72400ddbcecde170b668638cf224c5a4acb
y-jaga <yogeshjaga123@gmail.com>
2025-06-09 08:35:32 +0530

    added production configuration in config.config.js to run deployment

--- config/config.js
+++ config/config.js
@@ -8,4 +8,11 @@ module.exports = {
     host: process.env.DB_HOST,
     dialect: "postgres",
   },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+  },
 };
--- models/index.js
+++ models/index.js
@@ -11,28 +11,16 @@ const db = {};
 
 let sequelize;
 
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(
-//     config.database,
-//     config.username,
-//     config.password,
-//     config
-//   );
-// }
-
-sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  {
-    host: config.host,
-    dialect: "postgres",
-    port: config.port,
-  },
-  config
-);
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
 
 fs.readdirSync(__dirname)
   .filter((file) => {
